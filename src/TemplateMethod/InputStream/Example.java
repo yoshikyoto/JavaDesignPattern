1f763d771dd7a2904d09d160f0c15960834a5398
yoshiki_utakata <yoshikyoto@users.noreply.github.com>
2015-10-15 09:27:59 +0900

    Create Example.java

---
+++ src/TemplateMethod/InputStream/Example.java
@@ -0,0 +1,8 @@
+BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("hoge.txt")));
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+// 1文字読む
+br.read();
+
+// 1行読む
+br.readLine();
