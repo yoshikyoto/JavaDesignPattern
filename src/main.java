a83aba9ca67f422ee914874cbb0e274903cdca98
yoshiki_utakata <yoshikyoto@users.noreply.github.com>
2015-10-15 09:15:58 +0900

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,6 +3,8 @@ import TemplateMethod.Example.*;
 public class main {
 	public static void main(String args[]) {
 		AbstractDisplay dispChar = new CharDisplay('H');
+		// サブクラスの実装は気にせずに、
+		// display() を呼べばいい
 		dispChar.display();
 		
 		AbstractDisplay dispStr = new StringDisplay("Hello, World!");
